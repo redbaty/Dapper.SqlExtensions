c9893029a8ce8d2475a6b5381c1fd796eea2c2f1
Marcos Vinicius Maia <juninhowblu@gmail.com>
2018-04-23 21:35:20 -0300

    Added DbSetExtensions

---
+++ Dapper.SqlExtensions/DbsetExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dapper.SqlExtensions
+{
+    public static class DbsetExtensions
+    {
+        public static IEnumerable<T> UseDapper<T>(this DbSet<T> dbSet) where T : class
+        {
+            var context = dbSet.GetContext();
+            var sqlObject = new SqlObject(Activator.CreateInstance<T>(), context);
+            return context.Database.GetDbConnection()
+                .Query<T>(sqlObject
+                    .GetSelect());
+        }
+
+        public static DbContext GetContext<TEntity>(this DbSet<TEntity> dbSet)
+            where TEntity : class
+        {
+            return dbSet
+                .GetType()
+                .GetField("_context", BindingFlags.NonPublic | BindingFlags.Instance)
+                ?.GetValue(dbSet) as DbContext;
+        }
+    }
+}
